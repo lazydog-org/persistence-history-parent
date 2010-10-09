52dfc57adfb4c5459711696c98ec3fba6c9bc6b7
rjrjr <rjrjr@cox.net>
2010-10-09 00:55:56 +0000

    more work on history table stuff - rjrjr

--- history-table/src/main/java/org/lazydog/history/table/HistoryTable.java
+++ history-table/src/main/java/org/lazydog/history/table/HistoryTable.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Types;;
